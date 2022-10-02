2a3573345f296664037abb4f814d83e94a5679a9
Ruan Montelo <ruanmontelo@gmail.com>
2022-10-02 00:14:03 -0300

    Fixed bad collision handling and multiple collisions happening in the same frame, resulting in weird direction vectors

--- CircleEntity.cpp
+++ CircleEntity.cpp
@@ -3,12 +3,12 @@
 #include <SFML/Graphics.hpp>
 
 CircleEntity::CircleEntity(float radius, const sf::Vector2f& pos) {
-	m_direction = sf::Vector2f(1, 0);
+	m_direction = sf::Vector2f(0.5, 0.5);
 
 	m_shape = sf::CircleShape();
 	m_shape.setRadius(radius);
 	m_shape.setPosition(pos);
 
-	m_speed = 0.05f;
+	m_speed = 0.07f;
 }
 
--- main.cpp
+++ main.cpp
@@ -9,27 +9,30 @@ static const int WINDOW_WIDTH = 600;
 static const float BALL_RADIUS = 10.0f;
 static const float PI = 3.1415f;
 
-float getMagnitude(const sf::Vector2f& a) {
-	return sqrt((a.x * a.x) + (a.y * a.y));
+sf::Vector2f getRotated(const sf::Vector2f& vect) {
+	return sf::Vector2f(vect.y, vect.x * -1);
 }
 
-float getAngleBetweenTwoVectors(const sf::Vector2f& a, const sf::Vector2f& b) {
-	float a_mag = getMagnitude(a);
-	float b_mag = getMagnitude(b);
-
-	float times = (a.x * b.x) + (a.y * b.y);
-
-	return (acos(times / (a_mag * b_mag)) * 180) / PI;
+float getMagnitude(const sf::Vector2f& a) {
+	return sqrt((a.x * a.x) + (a.y * a.y));
 }
 
 sf::Vector2f getNormalizedVectorFromDirection(const float angle) {
 	return sf::Vector2f(cos(angle), sin(angle));
 }
 
+sf::Vector2f getNormalized(const sf::Vector2f& vect) {
+	float mag = getMagnitude(vect);
+	return sf::Vector2f(vect.x / mag, vect.y / mag);
+}
+
 bool collided(const sf::Shape& first_obj, const sf::Shape& second_obj) {
 	return first_obj.getGlobalBounds().intersects(second_obj.getGlobalBounds());
 }
 
+std::ostream& operator<<(std::ostream& stream, const sf::Vector2f& vector) {
+	return std::cout << vector.x << ", " << vector.y;
+}
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, 32U), "Breakout Clone");
@@ -40,12 +43,12 @@ int main() {
 
 	ball.m_shape.setFillColor(sf::Color::Red);
 
-	RectangleEntity rightWall(sf::Vector2f(1, WINDOW_HEIGHT), sf::Vector2f(WINDOW_WIDTH - 1, 1));
-	rightWall.m_shape.setFillColor(sf::Color::White);
-	rightWall.m_direction = sf::Vector2f(0, 1);
+	RectangleEntity right_wall(sf::Vector2f(1, WINDOW_HEIGHT), sf::Vector2f(WINDOW_WIDTH - 1, 1));
+	right_wall.m_shape.setFillColor(sf::Color::White);
+	right_wall.m_direction = sf::Vector2f(0, 1);
 	
-	RectangleEntity leftWall(sf::Vector2f(1, WINDOW_HEIGHT), sf::Vector2f(0, 0));
-	leftWall.m_direction = sf::Vector2f(0, 1);
+	RectangleEntity left_wall(sf::Vector2f(1, WINDOW_HEIGHT), sf::Vector2f(0, 0));
+	left_wall.m_direction = sf::Vector2f(0, 1);
 
 	RectangleEntity ceiling(sf::Vector2f(WINDOW_WIDTH, 1), sf::Vector2f(0, 0));
 	ceiling.m_direction = sf::Vector2f(1, 0);
@@ -70,22 +73,18 @@ int main() {
 			}
 		}
 
-		if (collided(ball.m_shape, rightWall.m_shape)) {
-			float angle = getAngleBetweenTwoVectors(ball.m_direction, rightWall.m_direction);
-			ball.m_shape.move(ball.getVelocity() * -1.0f);
-			ball.m_direction = getNormalizedVectorFromDirection(angle);
-		} else if (collided(ball.m_shape, leftWall.m_shape)) {
-			float angle = getAngleBetweenTwoVectors(ball.m_direction, leftWall.m_direction);
-			ball.m_shape.move(ball.getVelocity() * -1.0f);
-			ball.m_direction = getNormalizedVectorFromDirection(angle);
+		if (collided(ball.m_shape, right_wall.m_shape)) {
+			ball.m_direction = getRotated(ball.m_direction);
+			std::cout << "Collided with right wall. New direction: " << ball.m_direction << std::endl;
+		} else if (collided(ball.m_shape, left_wall.m_shape)) {
+			ball.m_direction = getRotated(ball.m_direction);
+			std::cout << "Collided with left wall. New direction: " << ball.m_direction << std::endl;
 		} else if (collided(ball.m_shape, player_entity.m_shape)) {
-			float angle = getAngleBetweenTwoVectors(ball.m_direction, player_entity.m_direction);
-			ball.m_shape.move(ball.getVelocity() * -1.0f);
-			ball.m_direction = getNormalizedVectorFromDirection(angle);
+			ball.m_direction = getRotated(ball.m_direction);
+			std::cout << "Collided with player entity. New direction: " << ball.m_direction << std::endl;
 		} else if (collided(ball.m_shape, ceiling.m_shape)) {
-			float angle = getAngleBetweenTwoVectors(ball.m_direction, ceiling.m_direction);
-			ball.m_shape.move(ball.getVelocity() * -1.0f);
-			ball.m_direction = getNormalizedVectorFromDirection(angle);
+			ball.m_direction = getRotated(ball.m_direction);
+			std::cout << "Collided with ceiling. New direction: " << ball.m_direction << std::endl;
 		}
 
 		ball.m_shape.move(ball.getVelocity());
@@ -94,9 +93,6 @@ int main() {
 
 		window.draw(player_entity.m_shape);
 		window.draw(ball.m_shape);
-		window.draw(rightWall.m_shape);
-		window.draw(leftWall.m_shape);
-		window.draw(ceiling.m_shape);
 
 		window.display();
 	}
